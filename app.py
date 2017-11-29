f718fd7de6c046091f861ce7e28797548b63066f
Zejian Shen <zesansan@gmail.com>
2017-11-29 12:17:18 -0800

    student crud works

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_modus import Modus
 
 app = Flask(__name__)
-app.config['SQLAlchemy_DATABASE_URI']='postgres://localhost/students-db'
-app.config['SQLAlchemy_TRACK_MODIFICATIONS'] = False 
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://localhost/students-db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app)
 modus = Modus(app)
 
@@ -12,8 +12,8 @@ class Student(db.Model):
 
 	__tablename__="students"
 
-	#columns, DDL
-	student_id = db.Column(db.Integer, primary_key =True)
+	#columns, DDLs
+	id = db.Column(db.Integer, primary_key =True)
 	first_name = db.Column(db.Text)
 	last_name = db.Column(db.Text)
 
@@ -24,7 +24,50 @@ class Student(db.Model):
 	
 	#making things readable	
 	def __repr__(self):
-		return "The student's name is {} {}.".format(self.first_name, self.last_name)	
+		return "The student's name is {} {}.".format(self.first_name, self.last_name)
+#route			
+@app.route('/')
+def root():
+	return redirect(url_for('index'))
+
+@app.route('/student', methods=['POST', 'GET'])
+def index():
+	if request.method == 'POST':
+		new_student = Student(
+			request.form['first_name'], 
+			request.form['last_name']
+			)
+		db.session.add(new_student)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template('index.html', students=Student.query.all())
+
+@app.route('/student/new')
+def new():
+	return render_template('new.html')
+
+@app.route('/student/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
+def show(id):
+	found_student = Student.query.get_or_404(id)
+	if request.method == b'PATCH':
+		found_student.first_name = request.form['first_name']
+		found_student.last_name = request.form['last_name']
+		db.session.add(found_student)
+		db.session.commit()
+
+	if request.method == b'DELETE':
+		db.session.delete(found_student)
+		db.session.commit()
+
+	if found_student and request.method == 'GET':
+		return render_template('show.html', student = found_student)
+
+	return render_template('show.html')
+
+@app.route('/student/<int:id>/edit')
+def edit(id):
+	found_student = Student.query.get_or_404(id)
+	return render_template('edit.html', student = found_student)
 
 if __name__ == '__main__':
 	app.run(debug=True)	
